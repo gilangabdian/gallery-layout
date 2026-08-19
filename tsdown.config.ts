7ba36f56345dad6e89f5bb633f2032b20f61e582
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-08-19 21:20:06 +0700

    first init

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  platform: "browser",
+  clean: true,
+  sourcemap: true,
+  css: {
+    inject: false
+  }
+});
